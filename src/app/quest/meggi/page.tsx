a292722d34fa62c95e10bb747d79241841f11249
helgasurojakova <91551597+helgasurojakova@users.noreply.github.com>
2024-04-21 16:29:05 +0300

    fix(quest pages): changed param name

--- src/app/quest/meggi/page.tsx
+++ src/app/quest/meggi/page.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { quests } from '@/config'
 
 export default function Quest() {
-  const collector = quests.find((el) => el.key === 'meggi')
+  const quest = quests.find((el) => el.key === 'meggi')
 
   return (
     <main className="bg-neutral-800 w-full">
       <section
-        className={`${collector?.backgroundImage} h-screen bg-center bg-no-repeat bg-cover`}
+        className={`${quest?.backgroundImage} h-screen bg-center bg-no-repeat bg-cover`}
       ></section>
     </main>
   )
