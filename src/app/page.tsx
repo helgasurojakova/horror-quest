a9db0c79263293f8b2672c1359afa26a15089939
helgasurojakova <91551597+helgasurojakova@users.noreply.github.com>
2024-04-21 14:13:44 +0300

    feat(main page): added contacts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import Image from 'next/image'
 import menu from '../../public/menu.svg'
 import { Rubik_Mono_One } from 'next/font/google'
+import email from '../../public/email.svg'
+import phone from '../../public/phone.svg'
+import telegram from '../../public/telegram.svg'
+import vk from '../../public/vk.svg'
+import whatsapp from '../../public/whatsapp.svg'
+import location from '../../public/location.svg'
+import { formatPhoneNumber } from '@/helpers'
 
 const rubikMono = Rubik_Mono_One({ weight: ['400'], subsets: ['latin'] })
 
@@ -26,7 +33,7 @@ const quests = [
 
 export default function Home() {
   return (
-    <main className="bg-neutral-800 w-screen">
+    <main className="bg-neutral-800 w-full">
       <nav className="flex justify-between bg-neutral-950 p-4 absolute w-full">
         <a className={`${rubikMono.className}`}>Квесты в Калининграде</a>
         <button>
@@ -34,10 +41,10 @@ export default function Home() {
         </button>
       </nav>
       <section className="bg-hero-pattern h-screen bg-center bg-no-repeat bg-cover"></section>
-      <section className="flex flex-col">
+      <section className="flex flex-col px-4 w-full gap-6 py-8">
         {quests.map((i, index) => {
           return (
-            <div key={index} className="m-4 bg-neutral-900">
+            <div key={index} className="bg-neutral-900">
               <div
                 className={`${i.backgroundImage} h-80 bg-no-repeat bg-cover bg-center relative`}
               >
@@ -50,6 +57,53 @@ export default function Home() {
           )
         })}
       </section>
+      <footer className="w-full p-4">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4 items-center">
+            <Image src={location} alt="Локация" className="w-6" />
+            <span>{process.env.REACT_APP_ADDRESS}</span>
+          </div>
+          <a
+            className="flex gap-4 items-center"
+            href={`tel:+${process.env.REACT_APP_PHONE_NUMBER}`}
+            target="_blank"
+          >
+            <Image src={phone} alt="Мобильный телефон" className="w-6" />
+            <span>{formatPhoneNumber(process.env.REACT_APP_PHONE_NUMBER)}</span>
+          </a>
+          <a
+            className="flex gap-4 items-center"
+            href={`mailto:${process.env.REACT_APP_EMAIL}`}
+            target="_blank"
+          >
+            <Image src={email} alt="Почта" className="w-6" />
+            <span>{process.env.REACT_APP_EMAIL}</span>
+          </a>
+          <div className="flex items-center gap-4">
+            <a
+              className="flex gap-4 items-center"
+              href={process.env.REACT_APP_VK_LINK}
+              target="_blank"
+            >
+              <Image src={vk} alt="VK" className="w-8" />
+            </a>
+            <a
+              className="flex gap-4 items-center"
+              href={`https://t.me/+${process.env.REACT_APP_PHONE_NUMBER}`}
+              target="_blank"
+            >
+              <Image src={telegram} alt="Telegram" className="w-8" />
+            </a>
+            <a
+              className="flex gap-4 items-center"
+              href={`https://wa.me/+${process.env.REACT_APP_PHONE_NUMBER}`}
+              target="_blank"
+            >
+              <Image src={whatsapp} alt="WhatsApp" className="w-8" />
+            </a>
+          </div>
+        </div>
+      </footer>
     </main>
   )
 }
