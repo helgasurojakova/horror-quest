67c8c2fed21715fc4867057ef556a3fc02ced197
helgasurojakova <91551597+helgasurojakova@users.noreply.github.com>
2024-04-23 01:54:47 +0300

    fix(quest card): changed styles

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -19,22 +19,22 @@ const Card: FC<Props> = (props) => {
   return (
     <div
       onClick={() => router.push(`quest/${data.key}`)}
-      className={`${
-        isActive && 'cursor-pointer'
-      } quest-card relative shadow-xl hover:grayscale-[0%] transition grayscale-[0%] xl:grayscale-[100%] lg:grayscale-[100%] md:grayscale-[0%] sm:grayscale-[0%] 
+      className="quest-card relative
       xl:w-4/12 lg:w-4/12 h-fit
       2xl:h-full lg:h-full
       pb-0 2xl:pb-48 lg:pb-48
-      `}
+      "
     >
       <div
         style={{ backgroundImage: `url(/${data.key}.jpg)` }}
-        className="h-80 2xl:h-full lg:h-full md:h-64 sm:h-80 bg-no-repeat bg-cover bg-center relative"
+        className={`${isActive && 'cursor-pointer'} card-image 
+        hover:grayscale-[0%] transition grayscale-[0%] xl:grayscale-[100%] lg:grayscale-[100%] md:grayscale-[0%] sm:grayscale-[0%]
+        shadow-xl h-80 2xl:h-full lg:h-full md:h-64 sm:h-80 bg-no-repeat bg-cover bg-center relative`}
       >
         <div className="absolute bottom-0 inside-card">
           <h1 className={`p-4 ${rubikMono.className}`}>{data.title}</h1>
           {isActive && (
-            <div className="shadow-xl intro-holder static overflow-hidden hidden 2xl:block lg:block">
+            <div className="intro-holder static overflow-hidden hidden 2xl:block lg:block">
               <p className="p-4 bg-neutral-900 leading-relaxed text-sm bg-opacity-70">
                 {data.intro}
               </p>
