a716264e596633f6b36f8d2f76e18e8bcb54d0b5
helgasurojakova <91551597+helgasurojakova@users.noreply.github.com>
2024-04-23 01:25:07 +0300

    fix(card): set intro into card for desktop

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -31,12 +31,19 @@ const Card: FC<Props> = (props) => {
         style={{ backgroundImage: `url(/${data.key}.jpg)` }}
         className="h-80 2xl:h-full lg:h-full md:h-64 sm:h-80 bg-no-repeat bg-cover bg-center relative"
       >
-        <h1 className={`absolute bottom-0 p-4 ${rubikMono.className}`}>
-          {data.title}
-        </h1>
+        <div className="absolute bottom-0 inside-card">
+          <h1 className={`p-4 ${rubikMono.className}`}>{data.title}</h1>
+          {isActive && (
+            <div className="shadow-xl intro-holder static overflow-hidden hidden 2xl:block lg:block">
+              <p className="p-4 bg-neutral-900 leading-relaxed text-sm bg-opacity-70">
+                {data.intro}
+              </p>
+            </div>
+          )}
+        </div>
       </div>
       {isActive && (
-        <div className="shadow-xl description-holder static xl:absolute lg:absolute md:static sm:static overflow-hidden">
+        <div className="shadow-xl intro-holder static xl:absolute lg:absolute md:static sm:static overflow-hidden">
           <p className="p-4 bg-neutral-900 leading-relaxed text-sm">
             {data.intro}
           </p>
