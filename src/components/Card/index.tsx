758372558a9ffd737f7adec7f1b91c5a6b7e14f6
helgasurojakova <91551597+helgasurojakova@users.noreply.github.com>
2024-04-22 23:06:43 +0300

    fix(card): changed responsive styles

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -21,18 +21,18 @@ const Card: FC<Props> = (props) => {
       onClick={() => router.push(`quest/${data.key}`)}
       className={`${
         isActive && 'cursor-pointer'
-      } quest-card relative hover:grayscale-[0%] transition grayscale-[0%] xl:grayscale-[100%] lg:grayscale-[100%] md:grayscale-[0%] sm:grayscale-[0%] xl:w-4/12 lg:w-4/12 h-fit`}
+      } quest-card relative shadow-xl hover:grayscale-[0%] transition grayscale-[0%] xl:grayscale-[100%] lg:grayscale-[100%] md:grayscale-[0%] sm:grayscale-[0%] xl:w-4/12 lg:w-4/12 h-fit`}
     >
       <div
         style={{ backgroundImage: `url(/${data.key}.jpg)` }}
-        className="h-80 bg-no-repeat bg-cover bg-center relative"
+        className="h-80 xl:h-80 lg:h-64 md:h-64 sm:h-80 bg-no-repeat bg-cover bg-center relative"
       >
         <h1 className={`absolute bottom-0 p-4 ${rubikMono.className}`}>
           {data.title}
         </h1>
       </div>
       {isActive && (
-        <div className="description-holder static xl:absolute lg:absolute md:static sm:static overflow-hidden">
+        <div className="shadow-xl description-holder static xl:absolute lg:absolute md:static sm:static overflow-hidden">
           <p className="p-4 bg-neutral-900 leading-relaxed text-sm">
             {data.description}
           </p>
