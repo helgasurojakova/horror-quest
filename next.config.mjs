d7222c2ee89efc39f1871e16c4ec8237e0cac3b4
helgasurojakova <91551597+helgasurojakova@users.noreply.github.com>
2024-04-22 12:00:36 +0300

    feat(app): added envs in next config (for client side)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    REACT_APP_PHONE_NUMBER: process.env.REACT_APP_PHONE_NUMBER,
+    REACT_APP_EMAIL: process.env.REACT_APP_EMAIL,
+    REACT_APP_VK_LINK: process.env.REACT_APP_VK_LINK,
+    REACT_APP_ADDRESS: process.env.REACT_APP_ADDRESS,
+    REACT_APP_YANDEX_MAP_LINK: process.env.REACT_APP_YANDEX_MAP_LINK,
+  }
+};
 
 export default nextConfig;
