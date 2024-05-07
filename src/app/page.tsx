2aefa257e8d4ed6a66d17729eadb32068d73331b
helgasurojakova <91551597+helgasurojakova@users.noreply.github.com>
2024-05-07 23:03:13 +0300

    refactor(main page): systematize class names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     const items = []
     for (let i = 0; i < count; i++) {
       items.push(
-        <div key={i} className="ticker_item py-2 pr-6 2xl:pr-8 md:pr-8">
+        <div key={i} className="ticker_item py-2 pr-6 md:pr-8">
           КЛУБ КВЕСТОВ КАЛИНИНГРАД
         </div>
       )
@@ -22,12 +22,12 @@ export default function Home() {
   return (
     <main>
       <section
-        className="bg-hero-pattern h-screen bg-center bg-no-repeat bg-cover 
-        flex flex-col justify-center items-center p-4 2xl:px-60 lg:px-40 md:px-20 sm:px-4
-        "
+        className="h-screen bg-hero-pattern bg-center bg-no-repeat bg-cover 
+        flex flex-col justify-center items-center
+        p-4 2xl:px-60 lg:px-40 md:px-20 sm:px-4"
       >
         <h1
-          className={`${rubikMono.className} w-full text-3xl 2xl:text-6xl md:text-4xl sm:text-4xl`}
+          className={`${rubikMono.className} w-full text-3xl 2xl:text-6xl sm:text-4xl`}
         >
           КЛУБ
           <br />
@@ -37,40 +37,43 @@ export default function Home() {
         </h1>
         <a
           href="#quests"
-          className="absolute bottom-20 border-2 rounded-md py-4 px-10 transition ease-in-out delay-150 active:bg-white active:text-neutral-950 hover:bg-white hover:text-neutral-950"
+          className="absolute bottom-20 border-2 rounded-md 
+          transition ease-in-out delay-150 active:bg-white active:text-neutral-950 hover:bg-white hover:text-neutral-950
+          py-4 px-10"
         >
           Перейти к просмотру
         </a>
       </section>
       <section
         id="quests"
-        className="
-        relative w-full overflow-hidden
+        className="relative overflow-hidden
         px-4 2xl:px-60 lg:px-40 md:px-20 sm:px-4
-        2xl:h-screen lg:h-screen
-        pt-8 2xl:pt-16 md:pt-16
-        pb-20 2xl:pb-24
-        "
+        w-full lg:h-screen
+        pt-8 md:pt-16
+        pb-20 2xl:pb-24"
       >
         <h2
           className={`${rubikMono.className} 
-          text-2xl 2xl:text-2xl md:text-2xl sm:text-xl 
-          pb-8 2xl:pb-16 sm:pb-8`}
+          text-xl sm:text-2xl
+          pb-8 md:pb-16`}
         >
           Во что поиграем..?
         </h2>
         <div
-          className="flex flex-col xl:flex-row lg:flex-row md:flex-col xs:flex-col gap-6 h-full
-          pb-0 2xl:pb-48 lg:pb-48
-        "
+          className="flex flex-col lg:flex-row gap-6 h-full
+          pb-0 lg:pb-48"
         >
           {quests.map((i, index) => {
             return <Card key={index} data={i} />
           })}
         </div>
-        <div className="absolute bottom-0 left-0 bg-neutral-900 mb-0 2xl:mb-14 lg:mb-14 md:mb-0">
+        <div
+          className="absolute bottom-0 left-0 
+          bg-neutral-900 
+          mb-0 lg:mb-14"
+        >
           <div className={`${rubikMono.className} ticker`}>
-            <div className="ticker_wrapper text-2xl 2xl:text-4xl md:text-4xl sm:text-2xl">
+            <div className="ticker_wrapper text-2xl lg:text-4xl">
               {tickerItems}
             </div>
           </div>
