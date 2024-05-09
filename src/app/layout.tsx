e84869a9155fe4b110fd864c4d129038580eee05
m.popov5 <m.popov@gazprombank.investments>
2024-05-10 00:10:24 +0300

    comment out object with metadata, use html meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ import { Metrika } from "@/components/Metrika";
 
 const rubik = Rubik({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Клуб квестов Калининград',
-  description: 'Погрузитесь в захватывающие приключения с лучшими перформанс хоррор квестами для всей семьи в Калининграде! Ощутите незабываемые эмоции!',
-}
+// export const metadata: Metadata = {
+//   title: 'Клуб квестов Калининград',
+//   description: 'Погрузитесь в захватывающие приключения с лучшими перформанс хоррор квестами для всей семьи в Калининграде! Ощутите незабываемые эмоции!',
+// }
 
 export default function RootLayout({
   children,
@@ -21,17 +21,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+        <title>Клуб квестов Калининград</title>
+        <meta name="description" content="Погрузитесь в захватывающие приключения с лучшими перформанс хоррор квестами для всей семьи в Калининграде! Ощутите незабываемые эмоции!" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" /> */}
-        <meta property="og:title" content={metadata.title} />
+        <meta property="og:title" content="Клуб квестов Калининград" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://questkaliningrad.ru/main.jpg" />
         <meta property="og:url" content="https://questkaliningrad.ru/" />
         <meta property="og:site_name" content="questkaliningrad.ru" />
-        <meta property="og:description" content={metadata.description} />
+        <meta property="og:description" content="Погрузитесь в захватывающие приключения с лучшими перформанс хоррор квестами для всей семьи в Калининграде! Ощутите незабываемые эмоции!" />
         <meta name="keywords" content="хоррор квесты, Калининград, приключения, развлечения, игры, тимбилдинг, командные игры, адреналин, решение головоломок, поиск выхода, тайны, загадки, интеллектуальные игры, развивающие игры, приключенческие игры, командная работа, ужасы, триллер, экстрим, испытание, выживание, зловещий, загадочный, темный, интрига, ужасающий, напряжение" />
       </head>
       <body className={rubik.className}>
